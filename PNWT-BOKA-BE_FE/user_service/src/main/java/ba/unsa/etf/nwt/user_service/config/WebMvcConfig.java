7df12df92a21b2fc9a0f6dffe6ddeaa13976eef5
mirnesa.salihovic <mirnesa.salihovic@evolt.dev>
2022-12-30 15:58:01 +0100

    virtual machine

--- PNWT-BOKA-BE_FE/user_service/src/main/java/ba/unsa/etf/nwt/user_service/config/WebMvcConfig.java
+++ PNWT-BOKA-BE_FE/user_service/src/main/java/ba/unsa/etf/nwt/user_service/config/WebMvcConfig.java
@@ -12,7 +12,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("*")
                 .allowedMethods("HEAD", "OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE")
                 .maxAge(MAX_AGE_SECS);
     }
